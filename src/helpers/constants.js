9370586dc86d8b380d46da5483d9d19d67ce310d
Connor Leggett <cjleggett@college.harvard.edu>
2021-02-17 13:14:20 -0400

    changed update workflow

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,8 @@
 const SERVER_URL = "https://virtual-meet-backend.herokuapp.com";
 // const SERVER_URL = "http://localhost:5000"
+const PRODUCTION = true
 
 module.exports = {
   SERVER_URL,
+  PRODUCTION,
 };
--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,15 +1,30 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import { PRODUCTION } from "./constants"
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
+const firebaseInfo = 
+  PRODUCTION ? {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  }
+  :
+  {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY_DEV,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_DEV,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL_DEV,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_DEV,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_DEV,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_DEV,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID_DEV,
+  }
+
+
+const app = firebase.initializeApp(firebaseInfo);
 
 export const auth = app.auth();
 export default app;
